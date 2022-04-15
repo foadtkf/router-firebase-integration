80076bba3e3c1388f9692d0fc77fd26a9c84d972
foadtkf <fakhrulif@gmail.com>
2022-04-15 10:18:27 +0600

    react-firebase-hook added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import useFirebase from './../../hooks/useFirebase';
 
 const Home = () => {
+    const {user}=useFirebase()
     return (
         <div>
             <h2>This is home</h2>
+            <h4>Name: {user? user.displayName:'no name'}</h4>
         </div>
     );
 };
